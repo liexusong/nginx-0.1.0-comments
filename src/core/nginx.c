582db501dd65232d16b930394e55e3509f6aa250
liexusong <liexusong@qq.com>
2015-02-11 20:58:32 +0800

    add comments

--- src/core/nginx.c
+++ src/core/nginx.c
@@ -3,34 +3,7 @@
  * Copyright (C) Igor Sysoev
  */
 
-/*
-#include <ngx_config.h>
-#include <ngx_core.h>
-
-extern ngx_module_t  ngx_core_module;
-extern ngx_module_t  ngx_errlog_module;
-extern ngx_module_t  ngx_conf_module;
-extern ngx_module_t  ngx_events_module;
-extern ngx_module_t  ngx_event_core_module;
-extern ngx_module_t  ngx_rtsig_module;
-extern ngx_module_t  ngx_epoll_module;
-extern ngx_module_t  ngx_http_module;
-extern ngx_module_t  ngx_http_core_module;
-extern ngx_module_t  ngx_http_log_module;
-extern ngx_module_t  ngx_http_static_module;
-extern ngx_module_t  ngx_http_index_module;
-extern ngx_module_t  ngx_http_access_module;
-extern ngx_module_t  ngx_http_proxy_module;
-extern ngx_module_t  ngx_http_write_filter_module;
-extern ngx_module_t  ngx_http_header_filter_module;
-extern ngx_module_t  ngx_http_chunked_filter_module;
-extern ngx_module_t  ngx_http_range_header_filter_module;
-extern ngx_module_t  ngx_http_charset_filter_module;
-extern ngx_module_t  ngx_http_userid_filter_module;
-extern ngx_module_t  ngx_http_headers_filter_module;
-extern ngx_module_t  ngx_http_copy_filter_module;
-extern ngx_module_t  ngx_http_range_body_filter_module;
-extern ngx_module_t  ngx_http_not_modified_filter_module;
+/* Which modules would load to Nginx
 
 ngx_module_t *ngx_modules[] = {
     &ngx_core_module,
@@ -59,6 +32,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_not_modified_filter_module,
     NULL
 };
+
 */
 
 #include <ngx_config.h>
@@ -66,13 +40,6 @@ ngx_module_t *ngx_modules[] = {
 #include <ngx_event.h>
 #include <nginx.h>
 
-
-/*
- * Nginx Http server must load modules:
- * 1) core
- * 2) event
- * 3) http
- */
 
 static ngx_int_t ngx_add_inherited_sockets(ngx_cycle_t *cycle);
 static ngx_int_t ngx_getopt(ngx_master_ctx_t *ctx, ngx_cycle_t *cycle);
