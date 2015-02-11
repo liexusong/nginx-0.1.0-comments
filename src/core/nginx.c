f5a8a4b800ec4fbb5afbff14429c2443e52869c9
unknown <liexusong@qq.com>
2015-02-12 01:52:53 +0800

    add comments

--- src/core/nginx.c
+++ src/core/nginx.c
@@ -45,11 +45,6 @@ ngx_module_t *ngx_modules[] = {
 };
 
  =========================================
- 1) core module
- 2) errlog module
- 3) configure module
- 4) event modules
- 5) http modules
 
   * Nginx会调用ngx_conf_parse()函数解析配置文件;
   * 当读到与模块相关的配置项时, Nginx会调用模块的相应接口;
@@ -58,12 +53,28 @@ ngx_module_t *ngx_modules[] = {
 
   # 四个主要调用:
   # ---------------------------------------------------------
-
-    main() -> ngx_init_cycle() -> ngx_master_process_cycle()
-                 \
-                  \___ ngx_conf_parse()
-
+  |
+  |  main() -> ngx_init_cycle() -> ngx_master_process_cycle()
+  |               \
+  |                \___ ngx_conf_parse()
+  |
   # ---------------------------------------------------------
+
+  ngx_init_cycle()函数解释:
+  =========================
+  * ngx_init_cycle()函数的作用是初始化一个新的生命周期,
+  * 在ngx_init_cycle()函数中会遍历所有的core模块,
+  * 并调用其create_conf()接口来创建配置上下文.
+  * 属于core模块的有5个, 如下:
+  *   ngx_core_module, ngx_errlog_module, ngx_conf_module, ngx_events_module, ngx_http_module
+  * 接着ngx_init_cycle()函数会调用ngx_conf_parse()函数来解析配置文件(下面会细说).
+  * 解析完配置文件后, 遍历所有的core模块, 并调用其init_conf()接口来初始化配置上下文.
+  * 然后nginx会遍历所有模块(注意是所有载入到nginx的模块), 并调用其init_module()接口来初始化模块.
+  * 当然除此之外, ngx_init_cycle()函数还会根据配置文件的配置来打开要监听的IP和端口.
+
+  ngx_conf_parse()函数解释:
+  =========================
+  
 
 #endif
 
